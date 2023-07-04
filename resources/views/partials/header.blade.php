1d0f8aa8e13e2391948d49ecec441f104f9eb9dc
FabrizioFavia <personal.favia@gmail.com>
2023-07-04 20:00:19 +0200

    added button route: create

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,4 +1,4 @@
 <header class="p-3 navbar d-flex px-5">
     <h1>COMIC-STORE</h1>
-    <button class="btn btn-primary me-5">Inserisci</button>
+    <a class="btn btn-primary me-5" href="{{route('comics.create')}}">Inserisci</a>
 </header>
