07b8e28bcc5371dd319e9dd85184ae44365bbbf1
FabrizioFavia <personal.favia@gmail.com>
2023-07-04 17:20:27 +0200

    created project / sent data to db

---
+++ database/migrations/2023_07_04_131301_create_comics_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comics', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->text('thumb');
+            $table->string('price', 6);
+            $table->string('series', 255);
+            $table->date('sale_date')->nullable();
+            $table->string('type', 30);
+            $table->text('artists', 255);
+            $table->string('writers', 255);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comics');
+    }
+};
